86f0c03465164055035b8a49c71b9dd744bc8961
Dave John Lucero <davejohn.lucero1@gmail.com>
2025-02-09 23:08:19 +0800

    Completed assignment: Created tables and inserted data

--- scripts/01_create_tables.sql
+++ scripts/01_create_tables.sql
@@ -10,3 +10,10 @@
 
     2. Place your create table query below this comment
 */
+
+   CREATE TABLE tblStudents (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(50),
+    student_email VARCHAR(50),
+    student_phone VARCHAR(50)
+);
